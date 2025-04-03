06e8587c1b5382088fc9404a019159f6c6df2525
potasium142 <potasium.1024@gmail.com>
2025-04-03 19:08:14 +0700

    hotfix/db port

--- conftest.py
+++ conftest.py
@@ -4,16 +4,17 @@ from testcontainers.postgres import PostgresContainer
 
 # stinky ass hack for test, i hate this
 def setup_test_container():
-    os.environ["DB_PORT"] = "6969"
     postgres = PostgresContainer(
         "docker.io/pgvector/pgvector:0.8.0-pg17",
-        port=6969,
         username="postgres",
         password="postgres",
         dbname="postgres",
-    ).with_bind_ports(6969, 6969)
+        port=5432,
+    ).with_bind_ports(5432, 5433)
 
     postgres.start()
+
+    os.environ["DB_PORT"] = "5433"
 
 
 def pytest_sessionstart(session):
